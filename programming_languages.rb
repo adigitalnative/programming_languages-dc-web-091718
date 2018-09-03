1cffcb132e230ea4a862c06c7670ab24ce9cab5c
Jacqueline Chenault <adigitalnative@gmail.com>
2018-09-03 00:09:04 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -13,10 +13,10 @@ def reformat_languages(languages)
   
   languages.each do |language_type, array_of_languages|
     array_of_languages.each do |language_name, language_details|
-      if reformatted_langugages.keys.include?(language_name)
-        reformatted_langugages[:language_name][:style] << language_type
+      if reformatted_languages.keys.include?(language_name)
+        reformatted_languages[:language_name][:style] << language_type
       else
-        reformatted_langugages[:language_name][:style] = [language_type]
+        reformatted_languages[:language_name][:style] = [language_type]
       end
     end
   end
