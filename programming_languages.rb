dbd7fce8bd9e5f0a52561914d84bf801a4f4513c
Jacqueline Chenault <adigitalnative@gmail.com>
2018-09-03 00:24:40 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,13 +4,6 @@ def reformat_languages(languages)
   # add the language type to the language in an array, as style: [:oo, :interpreted, :foo]
   reformatted_languages = {}
   
-  # Go through each language type
-  #  Go through each language in the type
-  #   If the language is already in the final array
-  #     add the language type to the final array[:language][:style]
-  #   Else
-  #     add the language to the final array w/ the language type included
-  
   languages.each do |language_type, array_of_languages|
     array_of_languages.each do |language_name, language_details|
       if reformatted_languages.keys.include?(language_name)
