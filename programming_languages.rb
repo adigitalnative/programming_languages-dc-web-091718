c5398d85709221ae9b377dac01978584d71f18a5
Jacqueline Chenault <adigitalnative@gmail.com>
2018-09-03 00:22:38 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -16,7 +16,7 @@ def reformat_languages(languages)
       if reformatted_languages.keys.include?(language_name)
         # reformatted_languages[:language_name][:style] << language_type
       else
-        reformatted_languages[language_name] = { type: language_details[:type], style: [language_type]
+        reformatted_languages[language_name] = { type: language_details[:type], style: [language_type] }
         binding.pry
       end
     end
