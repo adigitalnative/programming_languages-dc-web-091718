b2b322e0116df4503705ec13dc9e3259b3af255f
Jacqueline Chenault <adigitalnative@gmail.com>
2018-09-03 00:24:46 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,6 @@
 require 'pry'
 
 def reformat_languages(languages)
-  # add the language type to the language in an array, as style: [:oo, :interpreted, :foo]
   reformatted_languages = {}
   
   languages.each do |language_type, array_of_languages|
